de1c9bbee78bc8ddb7891bb04a58310a2bd9e47d
Christian Schreiber <christian@my365way.com>
2024-05-04 18:32:03 +0200

    General Update

--- app/api/cosmosdb/route.ts
+++ app/api/cosmosdb/route.ts
@@ -1,23 +1,33 @@
 import { NextResponse } from 'next/server'
 import { CosmosClient } from "@azure/cosmos";
 
-
 import { SecretClient } from '@azure/keyvault-secrets';
 import { DefaultAzureCredential } from '@azure/identity';
+
+let client: CosmosClient | undefined = undefined;
+
+const getCosmosClient = (connectionString: string): CosmosClient => {
+    if (!client) {
+      client = new CosmosClient(connectionString);
+    }
+    return client;
+  };
 
 export async function GET() {
     const credential = new DefaultAzureCredential();
     const url = process.env.AZURE_KEYVAULT_URL as string;
     const client = new SecretClient(url, credential);
-    const CosmosDBHost = await client.getSecret("CosmosDBHost");
-    const CosmosDBMasterKey = await client.getSecret("CosmosDBMasterKey");
+    const ConnectionString = await client.getSecret("ConnectionString");
+    const CosmosDB = await client.getSecret("CosmosDB");
+    const CosmosDBContainer = await client.getSecret("CosmosDBContainer");
 
-    const cosmosClient = new CosmosClient({
-        endpoint: CosmosDBHost.value,
-        key: CosmosDBMasterKey.value,
-    });
+    const container = getCosmosClient(ConnectionString.value).database(CosmosDB.value).container(CosmosDBContainer.value);
 
-    const { resources } = await cosmosClient.databases.readAll().fetchAll();
+    const resources = await container.items
+        .query({
+            query: "SELECT * FROM c"
+        })
+        .fetchAll();
 
     return NextResponse.json(resources);
 }
