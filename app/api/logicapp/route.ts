7a253aaa619c8131bdfe3605e8c369daa615e47c
Christian Schreiber <christian@my365way.com>
2024-05-05 21:03:46 +0200

    Update yaml

--- app/api/logicapp/route.ts
+++ app/api/logicapp/route.ts
@@ -1,24 +1,23 @@
 import { NextResponse } from 'next/server'
-//import fetch from 'node-fetch';
+import fetch from 'node-fetch';
 
-//import { SecretClient } from '@azure/keyvault-secrets';
-//import { DefaultAzureCredential } from '@azure/identity';
+import { SecretClient } from '@azure/keyvault-secrets';
+import { DefaultAzureCredential } from '@azure/identity';
 
 export async function GET() {
 
-    //const credential = new DefaultAzureCredential();
+    const credential = new DefaultAzureCredential();
 
-    //const url = process.env.AZURE_KEYVAULT_URL as string;
+    const url = process.env.AZURE_KEYVAULT_URL as string;
 
-    //const client = new SecretClient(url, credential);
+    const client = new SecretClient(url, credential);
 
-    //const Secret = await client.getSecret("LogicAppString");
+    const Secret = await client.getSecret("LogicAppString");
 
-    //const response = await fetch(Secret.value, {
-        //method: 'GET'
-    //});
-//
-    //const data = await response.text();
-    //return NextResponse.json(data);
-    return NextResponse.json({ message: 'Hello from the API!' });
+    const response = await fetch(Secret.value, {
+        method: 'GET'
+    });
+
+    const data = await response.text();
+    return NextResponse.json(data);
 }
