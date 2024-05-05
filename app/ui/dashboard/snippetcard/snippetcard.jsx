78d68d1417caf7086908e3026c4b7c1b4a6f5cfe
Christian Schreiber <christian@my365way.com>
2024-05-05 18:32:52 +0200

    Update CSS Style

--- app/ui/dashboard/snippetcard/snippetcard.jsx
+++ app/ui/dashboard/snippetcard/snippetcard.jsx
@@ -1,7 +1,27 @@
+import styles from "./snippetcard.module.css";
+
 const Snippetcard = ({ result }) => {
+  const data = result;
+  const hasData = data && data.resources && data.resources.length > 0;
+  const snippet = hasData ? data.resources[0] : null;
+
   return (
-    <div>
-      <pre>{JSON.stringify(result, null, 2)}</pre>
+    <div className={styles.wrapper}>
+        {result && (
+          <div className={styles.cardtext}>
+            {hasData && <h1 className={styles.element}>{snippet.snippetname}</h1>}
+            <h3>Description</h3>
+            {hasData && <p className={styles.element}>{snippet.longdescription}</p>}
+            <h3>Main Code</h3>
+            {hasData && <p className={styles.element}>{snippet.maincode}</p>}
+            <h3>Azure Modules</h3>
+            {hasData && <p className={styles.element}>{snippet.npmmodules}</p>}
+            <h3>Azure Ressources</h3>
+            {hasData && <p className={styles.element}>{snippet.azureressources && snippet.azureressources.join(' | ')}</p>}
+            <h3>Tags</h3>
+            {hasData && <p className={styles.element}>{snippet.tags && snippet.tags.join(' | ')}</p>}
+          </div>
+        )}
     </div>
   )
 }
